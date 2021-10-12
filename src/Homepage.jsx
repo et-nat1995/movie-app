1b0e807bad1fb6280d32ebfe4096837f4770e589
David Cohen <dovidcohen35@gmail.com>
2021-10-12 18:44:41 -0400

    styles for movie section

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -7,8 +7,17 @@ const TOKEN = process.env.REACT_APP_API_KEY;
 function Homepage() {
   const [movieData, setMovieData] = useState();
 
+  async function getMovies(type) {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${type}?api_key=${TOKEN}`
+    );
+    const json = await response.json();
+    const data = await json.results;
+    await setMovieData(data);
+  }
+
   useEffect(() => {
-    async function getPopular() {
+    async function getPopularMovies() {
       const response = await fetch(
         `https://api.themoviedb.org/3/movie/popular?api_key=${TOKEN}`
       );
@@ -16,17 +25,17 @@ function Homepage() {
       const data = await json.results;
       await setMovieData(data);
     }
-    getPopular();
+    getPopularMovies();
   }, []);
 
   return (
     <>
-    {/* ternary with if state=correct key selected-nav-item else regular-nav-item */}
-    {/* we will also display and setMovieData with this method -- think more about the search*/}
+      {/* ternary with if state=correct key selected-nav-item else regular-nav-item */}
+      {/* we will also display and setMovieData with this method -- think more about the search*/}
       <nav className="navbar">
         <button className="nav-btn">Search</button>
-        <button className="nav-btn">Upcoming Movies</button>
-        <button className="nav-btn">Popular Movies</button>
+        <button className="nav-btn" onClick={()=>getMovies("upcoming")}>Upcoming Movies</button>
+        <button className="nav-btn" onClick={()=>getMovies("popular")}>Popular Movies</button>
       </nav>
 
       <ul className="movie-section">
